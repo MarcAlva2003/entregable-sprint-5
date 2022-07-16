5029076d35c8a8f7f2f0eca6ac1bc0697d962204
LautaroProfumo <prolaucha8@gmail.com>
2022-07-15 23:42:33 -0300

    lo

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -24,21 +24,20 @@ class Cliente:
             numero = reader['numero'],
             dni = reader['dni'],
             direccion = reader['direccion'],
-            tipo = reader['tipo'])
-t
+            tipo = reader['tipo']),
+
         return cliente
 
-    def puede_crear_chequera(self):
+def puede_crear_chequera(self):
+        pass
+ 
+def puede_crear_tarjeta_credito(self):
         pass
 
-    def puede_crear_tarjeta_credito(self):
-        pass
-
-    def puede_comprar_dolar(self):
+def puede_comprar_dolar(self):
         pass
     
-        
-    def __repr__(self):
+def __repr__(self):
         return f"{self.nombre},{self.apellido},{self.tipo},{self.dni}"
 
            
